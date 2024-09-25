358a9177ac02d09b9030d3187da14d1e77cfff3b
Austin <austinjpoulsen@gmail.com>
2024-09-25 09:26:03 -0600

    label FORM error

--- index.js
+++ index.js
@@ -4,54 +4,60 @@ service_gak6c0m
 6ShYA1MSZ1nOo8o3j
 */
 
-function mouseBackground(event) {
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
 
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clentY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
+  }
 }
 
-
-let isModalOpen = false;
-let contrastToggle = false; 
-
 function toggleContrast() {
-    contrastToggle = !contrastToggle;
-    if (contrastToggle){
-        document.body.classList += " dark-theme"
-    }
-    else {
-        document.body.classList.remove("dark-theme")
-    }
-
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme";
+  } else {
+    document.body.classList.remove("dark-theme");
+  }
 }
 
 function contact(event) {
-    event.preventDefault();
-    const loading = document.querySelector('.modal__overlay--loading')
-    const success = document.querySelector('.modal__overlay--success')
-    loading.classList += " modal__overlay--visible";
-    emailjs
-        .sendForm(
-            'service_gak6c0m',
-            'template_s3chdi9',
-            event.target,
-            '6ShYA1MSZ1nOo8o3j'
-        ).then(() => {
-            loading.classList.remove("modal__overlay--visible");
-            success.classList += " modal__overlay--visible";
-        })
-        .catch(() => {
-            loading.classList.remove("modal__overlay--visible");
-            alert(
-                "the email service is temporarily unavailable. Please contact me directly at: austinjpoulsen@gmail.com Thank you!"
-            );
-        })
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_gak6c0m",
+      "template_s3chdi9",
+      event.target,
+      "6ShYA1MSZ1nOo8o3j"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "the email service is temporarily unavailable. Please contact me directly at: austinjpoulsen@gmail.com Thank you!"
+      );
+    });
 }
 
-
 function toggleModal() {
-    if (isModalOpen) {
-        isModalOpen = false; 
-        return document.body.classList.remove("modal--open")
-    }
-    isModalOpen = true;
-    document.body.classList += " modal--open"
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
 }
